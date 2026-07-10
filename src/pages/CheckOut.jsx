d58befae71b3addf5840164157d97ccd1037dca7
Anees-Rahman-dev <aneesrahman0101@gmail.com>
2026-07-10 21:58:17 +0530

    added items center for checkout

--- src/pages/CheckOut.jsx
+++ src/pages/CheckOut.jsx
@@ -87,7 +87,7 @@ export default function CheckOut() {
   }
 
   return (
-    <div className="max-w-4xl mx-auto p-6 grid md:grid-cols-2 gap-10 pt-30">
+    <div className="flex flex-col  min-h-[500px] items-center max-w-4xl mx-auto p-6 grid md:grid-cols-2 gap-10 pt-30">
       {/* Address Form */}
       <div>
         <h2 className="text-2xl font-bold text-gray-800 mb-6">Delivery Address</h2>
